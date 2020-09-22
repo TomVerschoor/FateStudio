8ace436b29fb9e888e4a0f4db24b44171846a207
tomve <tomve@DESKTOP-GI8H2RM>
2020-09-22 11:52:33 +0200

    Added player stats

---
+++ Music RPG/Assets/battleSystem/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currentHP { get; private set; }
+
+    public Stat danage;
+
+    private void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage");
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
