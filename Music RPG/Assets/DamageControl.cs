d327e0c03db56e669614231111b394065f9c9f3c
tomve <tomve@DESKTOP-GI8H2RM>
2021-02-05 20:11:43 +0100

    backup

---
+++ Music RPG/Assets/DamageControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageControl : MonoBehaviour
+{
+    public double damage = 1;
+    public static bool damaged = false;
+
+    public void setDamage(double dmg)
+    {
+        damage = dmg;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("COL");
+        if (col.gameObject.name == "Player" && !damaged)
+        {
+            damaged = true;
+            Stats.Instance.TakeDamange(damage);
+            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Synthia/Hurt/Hurt SFX", GetComponent<Transform>().position);
+            //Destroy(gameObject);
+            //Debug.Log("Shockwave hit! Player:"); //+ArenaController.lives
+        }
+    }
+
+}
---
+++ Music RPG/Assets/Fairy_HP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fairy_HP : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Collision detected");
+        if (other.name == "Player")
+        {
+
+            {
+
+                Stats.Instance.TakeDamange(-2);
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
